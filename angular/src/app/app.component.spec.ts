a20f6e05135a77991dcab112580c82c98f28d151
zriyansh <prik2two@gmail.com>
2023-06-23 23:47:03 +0530

    updated angular code

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hello-world-app'`, () => {
+  it(`should have as title 'sample-angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-world-app');
+    expect(app.title).toEqual('sample-angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hello-world-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sample-angular-app app is running!');
   });
 });
